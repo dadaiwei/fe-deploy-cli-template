3dd99133169ce6d7d0570c6514881fbec46bb283
dadaiwei <dadaiwei@corp.netease.com>
2020-01-10 19:14:00 +0800

    ADD: Update deploy template.

--- deploy.config.js
+++ deploy.config.js
@@ -4,19 +4,23 @@ module.exports = {
   projectName: '', // 项目名称
   dev: { // 测试环境
     name: '测试环境',
+    script: "npm run build-dev", // 测试环境打包脚本
     host: '', // 开发服务器地址
     port: 22, // ssh port，一般默认22
     username: '', // 登录服务器用户名
     password: '', // 登录服务器密码
-    script: "npm run build", // 打包脚本
+    distPath: 'dist',  // 本地打包dist目录
+    webDir: '',  // // 测试环境服务器地址
   },
-  prod: { // 线上环境
+  prod: {  // 线上环境
     name: '线上环境',
+    script: "npm run build", // 线上环境打包脚本
     host: '', // 开发服务器地址
     port: 22, // ssh port，一般默认22
     username: '', // 登录服务器用户名
     password: '', // 登录服务器密码
-    script: "npm run build", // 打包脚本
+    distPath: 'dist',  // 本地打包dist目录
+    webDir: '' // 线上环境web目录
   }
-  // 再还有多余的环境按照上面格式写即可
+  // 再还有多余的环境按照这个格式写即可
 }
