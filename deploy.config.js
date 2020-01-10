44023888979d666bde71a3b35f8fc3438cbe95a5
dadaiwei <dadaiwei@corp.netease.com>
2020-01-10 12:16:47 +0800

    ADD:Upload deploy template.

---
+++ deploy.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  privateKey: '', // 本地私钥地址，位置一般在C:/Users/xxx/.ssh/id_rsa
+  passphrase: '', // 本地私钥密码
+  projectName: '', // 项目名称
+  dev: { // 测试环境
+    host: '', // 开发服务器地址
+    port: 22, // ssh port，一般默认22
+    username: '', // 登录服务器用户名
+    password: '', // 登录服务器密码
+    script: "npm run build", // 打包脚本
+  },
+  prod: { // 线上环境，再还有多余的环境按照这个格式写即可
+    host: '', // 开发服务器地址
+    port: 22, // ssh port，一般默认22
+    username: '', // 登录服务器用户名
+    password: '', // 登录服务器密码
+    script: "npm run build", // 打包脚本
+  }
+}
