44572925ea81a6c67b35db2ed359358c805eaafc
dadaiwei <dadaiwei@corp.netease.com>
2020-01-10 12:27:46 +0800

    FEAT:Update deploy.config.js

--- deploy.config.js
+++ deploy.config.js
@@ -3,17 +3,20 @@ module.exports = {
   passphrase: '', // 本地私钥密码
   projectName: '', // 项目名称
   dev: { // 测试环境
+    name: '测试环境',
     host: '', // 开发服务器地址
     port: 22, // ssh port，一般默认22
     username: '', // 登录服务器用户名
     password: '', // 登录服务器密码
     script: "npm run build", // 打包脚本
   },
-  prod: { // 线上环境，再还有多余的环境按照这个格式写即可
+  prod: { // 线上环境
+    name: '线上环境',
     host: '', // 开发服务器地址
     port: 22, // ssh port，一般默认22
     username: '', // 登录服务器用户名
     password: '', // 登录服务器密码
     script: "npm run build", // 打包脚本
   }
+  // 再还有多余的环境按照上面格式写即可
 }
